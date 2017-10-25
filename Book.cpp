33102ff9a5a72470fb54400033b4d2e5e51c2960
Dongmantc <1527728702@qq.com>
2017-10-25 22:58:27 +0800

    hjhj

--- Book.cpp
+++ Book.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main()
 {
-    int i;
+    int i,j;
     cout<<"hello,world"<<endl;
     cout<<"test"<<endl;
     for(i=0;i<10;i++)
-        cout<<i <<endl;
+        for(j=0;j<10;j++)
+            cout<<i <<endl;
     return 0;
 }
