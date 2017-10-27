fc46be91923dd73dd15e99ec03b427862a5edb96
Eureka000 <281044541@qq.com>
2017-10-27 10:35:29 +0800

    ..

--- Book.cpp
+++ Book.cpp
@@ -15,40 +15,39 @@ Private：
 
 Class Library {
 Public:
-Void charge(Card card);//(充值）
-Void bookLend(Book book);//(借书）
-Void bookReturn(Book book);//(还书）
-Void bookOrder(Book book);//（预约）
-Void bookRenew(Book book);//（续借）
+    Void charge(Card card);//(充值）
+    Void bookLend(Book book);//(借书）
+    Void bookReturn(Book book);//(还书）
+    Void bookOrder(Book book);//（预约）
+    Void bookRenew(Book book);//（续借）
 }
 
 Class Administrator {
 Public:
-Void addBook();
-Void deleteBook();
-Void newStorage();
-Void searchRecord();
-Void operateCard();
+    Void addBook();
+    Void deleteBook();
+    Void newStorage();
+    Void searchRecord();
+    Void operateCard();
 Private:
 
 }
 
 Class Record {
 Public:
-Void login(){
-	cout<<endl;
-};
-Void register();
-Void updatePassword();
+    Void login() {
+        cout<<endl;
+    };
+    Void register();
+    Void updatePassword();
 
 
 Private:
 
 }
 
-int main()
-{
-	cout << "恕我直言，大家都是胖子~";
-	cout<<"%%%%%%%"<<endl<<endl;、
-	cout<<"wangzhengtao";
+int main() {
+    cout << "恕我直言，大家都是胖子~";
+    cout<<"%%%%%%%"<<endl<<endl;
+    cout<<"wangzhengtao";
 }
