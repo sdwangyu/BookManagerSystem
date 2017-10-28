d7a35336f2e39eed1dfdf917ceddab7a35201752
Wangzhengtao <sdwangyu97@126.com>
2017-10-28 10:42:27 +0800

    图书规划 cpp细微修改

--- book.cpp
+++ book.cpp
@@ -72,7 +72,7 @@ Public:
 // 需要编写
     Private：
     char cardID[11];
-    char aPassword[20];
+    char cPassword[20];
     short lendedCount;//已借本数
     short lendingCount;//可借本数
     char cardHolder[10];
@@ -112,11 +112,11 @@ Public:
     void searchRecord();
     void operateCard(Card card);
 
-    char cardID[11];
+    char account[11];
     char aPassword[20];
-    char cardHolder[10];
-    char cID[18];
-    char cPhone[11];
+    char accountHolder[10];
+    char aID[18];
+    char aPhone[11];
 
 }
 
