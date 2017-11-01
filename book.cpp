630b0e9cdb4aaf0ed08df6b343a3c7019b3b666b
zxfCarrie <17806230802@163.com>
2017-11-02 00:09:58 +0800

    新增日期比较函数

--- book.cpp
+++ book.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 #include<stdio.h>
 #include<string>
 #include<time.h>
@@ -25,7 +25,48 @@ using namespace std;
 int allcard;//从文件中读取 修改后重新写入文件  用户注册 ++
 int allbook;//增加图书 ++
 
-int compareDate();//匹配
+int isLeapYear(int year){//判断是否是闰年,返回1为闰年，返回0不是闰年
+    if(year%4==0&&year%100!=0)return 1;
+    if(year%400==0)return 1;
+    return 0;
+}
+
+int getDayInYear(int year,int month,int day){//得到该日期是一年的第几天
+    int months[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    if(isLeapYear(year))months[1]=29;
+    for(int i=0;i<month-1;i++){
+        day=day+months[i];
+    }
+    return day;
+}
+
+int compareDate(int year1,int month1,int day1,int year2,int month2,int day2){//日期比较,日期1year1month1day1-日期2year2month2day2,
+    int days=0;
+    //不需要判断日期数字是否合理，系统日期不会出现32天的情况
+    if(year1==year2){//年份相等
+        if(month1==month2)days=day1-day2;//月份相等
+        else days=getDayInYear(year1,month1,day1)-getDayInYear(year2,month2,day2);//月份不等
+    }
+    else {//年份不同，则日期差值为日期1的在year1的第几天+日期2在year2还剩下的天数+year1和year2两年之间的天数差值
+        //确保year1年份比year2晚
+         if(year1 > year2){
+            if(isLeapYear(year2))
+                days= 366 - getDayInYear(year2,month2, day2); //取得日期2在该年还剩下多少天
+            else
+                days= 365 - getDayInYear(year2,month2, day2);
+            days += getDayInYear(year1,month1,day1); //取得日期1在当年中的第几天
+            for(int year = year2 + 1; year < year1; year++)//取得year1和year2之间的天数差值
+            {
+                if(isLeapYear(year))
+                    days+= 366;
+                else
+                    days+= 365;
+            }
+         }
+    }
+    if(days<0)exit(0);//出错则退出程序
+    return days;
+}
 
 class Book//构造函数 复制构造函数
 {
@@ -208,7 +249,7 @@ Public:
             cardHolder[i]=CardHolder[i];
         }
         lendedCount=0;//初始已借本数为0
-        lendingCount=10;//初始可借本数为10        
+        lendingCount=10;//初始可借本数为10
         bookState='1';//1表示未冻结
         balance=Balance;
         ownMoney=0;
@@ -269,13 +310,13 @@ Public:
         for(i=0;i<18;i++)
         {
             cID[i]=' ';
-        }    
+        }
         for(i=0;i<11;i++)
         {
             cPhone=' ';
         }
         lendedCount=0;//初始已借本数为0
-        lendingCount=10;//初始可借本数为10        
+        lendingCount=10;//初始可借本数为10
         bookState='1';//1表示未冻结
         ownMoney=0;
         bookedCount=0;//初始预约本数为0
@@ -411,14 +452,14 @@ Public:
 		flag2 =flag22;
         //获取当前系统日期 自行查询方法 读入当前year month day
     }
-	Record(Card card1, int Year, int Month, int Day, char flag1) 
+	Record(Card card1, int Year, int Month, int Day, char flag1)
 	{
 		card = card2;
 		Year = year;
 		Month = month;
 		Day = day;
-		
-	
+
+
 	}
 	//复制构造函数
 	Record(Record &R);
@@ -433,7 +474,7 @@ Public:
     void signOutRecord();//注销记录
 	void signUpRecord();//注册记录
 
-	char getflag1() 
+	char getflag1()
 	{
 		return flag1;
 	}
@@ -454,7 +495,7 @@ Public:
 		return flag2;
 	}
 	private：
-    char flag1;  //a借书 b还书 c预约 d续借 e取消预约 f预约失效 g预约记录  
+    char flag1;  //a借书 b还书 c预约 d续借 e取消预约 f预约失效 g预约记录
     Book book;
     Card card;
     int year;
@@ -682,7 +723,7 @@ void Library::signInUser(char*username_PutIn, char*password_PutIn){		//用户登
 		printf("file error\n");
 		exit(1);
 	}
-	
+
 	Card card_temp;
 	int i = 0;	//循环变量，用于将fp向后移动
 	while (fp != fpEnd){
